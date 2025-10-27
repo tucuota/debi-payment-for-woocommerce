5942b972a090d62bde93ecd479ae80404fcbc3ee
fernandodelperal <31489588+fernandodelperal@users.noreply.github.com>
2025-10-27 04:07:22 -0300

    cleanup

--- debi-payment-for-woocommerce.php
+++ debi-payment-for-woocommerce.php
@@ -45,6 +45,11 @@ if (woocommerce_debi_is_woocommerce_active()) {
 		return $gateways;
 	}
 
+	add_action('plugins_loaded', 'init_woocommerce_debi', 10);
+	function init_woocommerce_debi() {
+		require_once plugin_dir_path(__FILE__) . 'class-wc-debi.php';
+	}
+
 }
 
 /**
